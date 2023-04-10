cad308ba475615112fe29793348d288a3cf3c0e6
OJI <127828021+OJI01@users.noreply.github.com>
2023-04-10 22:50:12 +0700

    Update Numbers.cs

--- Numbers.cs
+++ Numbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,43 +14,37 @@ namespace pert5
             Console.WriteLine("Nilai terkecil antara {0}, {1}, {2}", a, b, c);
             //Melakukan dua perkondisian di setiap if
             if (a <= b && a <= c)
-            {
-                //output yang akan di keluarkan jika memenuhi syarat
-                Console.WriteLine("Adalah: {0}", a);
+            {               
+                Console.WriteLine("Nilai terkecilnya : {0}", a);
             }
             else if (b <= a && b <= c)
-            {
-                //output yang akan di keluarkan jika memenuhi syarat
-                Console.WriteLine("Adalah: {0}", b);
+            {               
+                Console.WriteLine("Nilai terkecilnya : {0}", b);
             }
             else if (c <= a && c <= b)
             {
-                //output yang akan di keluarkan jika memenuhi syarat
-                Console.WriteLine("Adalah: {0}", c);
-
+                Console.WriteLine("Nilai terkecilnya : {0}", c);
             }
         }
 
         //Menggunakan method non void yang memeliki 3 variabel inputan
         public int nilaiMax(int a, int b, int c)
         {
-            Console.WriteLine("Nilai terbesar antara {0}, {1}, {2} adalah :", a, b, c);
+            Console.WriteLine("Nilai terbesar antara : {0}, {1}, {2}", a, b, c);
             //Melakukan dua perkondisian di setiap if
             if (a >= b && a >= c)
             {
-                //output yang akan di keluarkan jika kondisi memenuhi syarat
-                Console.WriteLine("Adalah: {0}", a);
+                Console.WriteLine("Nilai terbesarnya : {0}", a);
             }
             else if (b >= a && b >= c)
             {
-                //output yang akan di keluarkan jika kondisi memenuhi syarat
-                Console.WriteLine("Adalah: {0}", b);
+                Console.WriteLine("Nilai terbesarnya : {0}", b);
             }
             else if (c >= a && c <= b)
             {
-                //output yang akan di keluarkan jika kondisi memenuhi syarat
-                Console.WriteLine("Adalah: {0}", c);
+                Console.WriteLine("Nilai terbesarnya : {0}", c);
             }
             return 0;
         }
-
+    }
+}
